f2ca8a6a2cb1f0de65ed6ea55a20c529c9b2098c
zijunn93 <34432534+zijunn93@users.noreply.github.com>
2020-04-20 23:15:32 +0800

    add font smoothing

--- app/global-styles.js
+++ app/global-styles.js
@@ -10,6 +10,8 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+	  -moz-osx-font-smoothing: grayscale;
   }
 
   body.fontLoaded {
