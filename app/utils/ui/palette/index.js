633dd8f1af761949d9d98c7d2d2696ad5bd55b3a
zijunn93 <34432534+zijunn93@users.noreply.github.com>
2020-04-20 22:45:37 +0800

    add palette config

---
+++ app/utils/ui/palette/index.js
@@ -0,0 +1,24 @@
+export const primary = {
+  main: '#e8511d',
+  darker: '#ce4415',
+  dark: '#dc653c',
+  light: '#4f4f9c',
+  white: '#ffffff',
+};
+
+export const textColor = {
+  primary: '#1d1d1b',
+  secondary: '#565654',
+  tertiary: '#a0a0a0',
+  inverse: '#fffff',
+  error: '#d40000',
+  main: '#e8511d',
+  disabled: '#dddddd',
+};
+
+const palette = {
+  primary,
+  textColor,
+};
+
+export default palette;
