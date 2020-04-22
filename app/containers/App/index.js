181a06e4d1b7368cf890f3e2c73ef2015ab4290e
zijunn93 <34432534+zijunn93@users.noreply.github.com>
2020-04-22 22:59:47 +0800

    update app wrapper

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -21,11 +21,11 @@ import LoginPage from 'containers/LoginPage/Loadable';
 import GlobalStyle from '../../global-styles';
 
 const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
+  // max-width: calc(768px + 16px * 2);
   margin: 0 auto;
   display: flex;
   min-height: 100%;
-  padding: 0 16px;
+  // padding: 0 16px;
   flex-direction: column;
 `;
 
