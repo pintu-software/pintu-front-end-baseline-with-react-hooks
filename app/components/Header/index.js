6e97716c313de74f82a9439c0c90e7814fb87efc
zijunn93 <34432534+zijunn93@users.noreply.github.com>
2020-04-22 22:59:27 +0800

    update header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,28 +1,113 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+// import { FormattedMessage } from 'react-intl';
 
-import A from './A';
-import Img from './Img';
-import NavBar from './NavBar';
-import HeaderLink from './HeaderLink';
-import Banner from './banner.jpg';
-import messages from './messages';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+// import messages from './messages';
+import Logo from './assets/logo.png';
+
+const Wrapper = styled.div`
+  display: block;
+  width: 100%;
+  height: 60px;
+  position: sticky;
+`;
+
+const StyledHeader = styled.header`
+  display: flex;
+  height: inherit;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  background: #ffffff;
+  box-shadow: 0 4px 8px 0 rgba(48, 82, 120, 0.03);
+  padding-left: 24px;
+  padding-right: 24px;
+`;
 
 function Header() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = evt => {
+    setAnchorEl(evt.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div>
-      <A href="https://www.reactboilerplate.com/">
-        <Img src={Banner} alt="react-boilerplate - Logo" />
-      </A>
-      <NavBar>
-        <HeaderLink to="/">
-          <FormattedMessage {...messages.home} />
-        </HeaderLink>
-        <HeaderLink to="/features">
-          <FormattedMessage {...messages.features} />
-        </HeaderLink>
-      </NavBar>
-    </div>
+    <Wrapper>
+      <StyledHeader>
+        <div
+          style={{ display: 'flex', alignItems: 'center', paddingLeft: '20px' }}
+        >
+          <Link to="/">
+            <img
+              src={Logo}
+              alt="Pintu - Doorway to solution"
+              width="36"
+              height="36"
+            />
+          </Link>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flex: 1,
+            alignItems: 'center',
+            paddingLeft: '20px',
+          }}
+        >
+          {/* <p>Dashboard</p>
+          <p>Budgets</p> */}
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            paddingRight: '20px',
+          }}
+        >
+          <Button
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <AccountCircle fontSize="large" />
+            <p style={{ margin: '0 7px' }}>Test User</p>
+            <ExpandMore />
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            elevation={0}
+            getContentAnchorEl={null}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          >
+            <MenuItem style={{ width: '160px' }} onClick={handleClose}>
+              Profile
+            </MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
+        </div>
+      </StyledHeader>
+    </Wrapper>
   );
 }
 
