38f3f9e0f9dcdcc50fc33ff8654043ec8c42c6eb
zijunn93 <34432534+zijunn93@users.noreply.github.com>
2020-04-23 18:19:21 +0800

    update app wrapper

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -24,7 +24,8 @@ const AppWrapper = styled.div`
   // max-width: calc(768px + 16px * 2);
   margin: 0 auto;
   display: flex;
-  min-height: 100%;
+  // 120px is the height of header and footer
+  min-height: calc(100vh - 120px);
   padding: 0 16px;
   flex-direction: column;
 
