8659f3ac3d2ef018cec2f8d0f90f2294b0f3936c
zijunn93 <34432534+zijunn93@users.noreply.github.com>
2020-04-23 16:21:12 +0800

    add typography config

--- app/utils/ui/theme/index.js
+++ app/utils/ui/theme/index.js
@@ -60,6 +60,40 @@ const theme = createMuiTheme({
         },
       },
     },
+    MuiTypography: {
+      h1: {
+        fontSize: '40px',
+      },
+      h2: {
+        fontSize: '28px',
+      },
+      h3: {
+        fontSize: '24px',
+      },
+      h4: {
+        fontSize: '20px',
+      },
+      h5: {
+        fontSize: '16px',
+      },
+      h6: {
+        fontSize: '14px',
+      },
+      body1: {
+        fontSize: '16px',
+      },
+      body2: {
+        fontSize: '14px',
+      },
+      subtitle1: {
+        fontSize: '16px',
+        fontWeight: 'bold',
+      },
+      subtitle2: {
+        fontSize: '14px',
+        fontWeight: 'bold',
+      },
+    },
   },
   /** You can change the default props of all the Material-UI components. A props key is exposed in the theme for this use case. */
   props: {
