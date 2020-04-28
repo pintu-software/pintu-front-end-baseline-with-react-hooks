5a639e57fd06c29c69ff847d682e0e022c655164
zijunn93 <34432534+zijunn93@users.noreply.github.com>
2020-04-28 15:13:38 +0800

    add guide

--- app/containers/LoginPage/reducer.js
+++ app/containers/LoginPage/reducer.js
@@ -1,6 +1,9 @@
 /*
+ * LoginPage Reducer
  *
- * LoginPage reducer
+ * The reducer takes care of our data. Using actions, we can
+ * update our application state. To add a new action,
+ * add it to the switch statement in the reducer function
  *
  */
 import produce from 'immer';
