f4d11d188abb3f5ba048d7eea6ff5b20a046a87b
zijunn93 <34432534+zijunn93@users.noreply.github.com>
2020-04-28 15:41:28 +0800

    add guide

--- app/containers/LoginPage/saga.js
+++ app/containers/LoginPage/saga.js
@@ -18,6 +18,14 @@ export function* requestLogin({ payload }) {
   }
 }
 
+/**
+ * Root saga manages watcher lifecycle
+ */
 export default function* loginPageSaga() {
+  // Watches for LOAD_REPOS actions and calls getRepos when one comes in.
+  // By using `takeLatest` only the result of the latest API call is applied.
+  // It returns task descriptor (just like fork) so we can continue execution
+  // It will be cancelled automatically on component unmount
+
   yield takeLatest(LOGIN, requestLogin);
 }
