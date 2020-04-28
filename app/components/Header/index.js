81dff5f2bf9d745c91a52aeda91ee5de157c7c01
zijunn93 <34432534+zijunn93@users.noreply.github.com>
2020-04-28 14:00:43 +0800

    update header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 // import { FormattedMessage } from 'react-intl';
 import { requestLogout } from 'containers/LoginPage/actions';
@@ -50,6 +50,55 @@ function Header({ isAuthUser, onRequestLogout }) {
     setAnchorEl(null);
   };
 
+  const handleLogout = () => {
+    handleClose();
+    onRequestLogout();
+  };
+
+  const history = useHistory();
+  const location = useLocation();
+  const isInLoginPage = location.pathname === '/login';
+
+  const authUserSection = (
+    <>
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+        style={{ backgroundColor: 'transparent', color: 'black' }}
+      >
+        <AccountCircle fontSize="large" />
+        <p style={{ margin: '0 7px' }}>Test User</p>
+        <ExpandMore />
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <MenuItem style={{ width: '222px' }} onClick={handleLogout}>
+          Logout
+        </MenuItem>
+      </Menu>
+    </>
+  );
+
+  const guestSection = isInLoginPage ? null : (
+    <Button onClick={() => history.push('/login')}>Login</Button>
+  );
+
   return (
     <Wrapper>
       <StyledHeader>
@@ -79,44 +128,7 @@ function Header({ isAuthUser, onRequestLogout }) {
             alignItems: 'center',
           }}
         >
-          {isAuthUser ? (
-            <>
-              <Button
-                aria-controls="simple-menu"
-                aria-haspopup="true"
-                onClick={handleClick}
-                style={{ backgroundColor: 'transparent', color: 'black' }}
-              >
-                <AccountCircle fontSize="large" />
-                <p style={{ margin: '0 7px' }}>Test User</p>
-                <ExpandMore />
-              </Button>
-              <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-                elevation={0}
-                getContentAnchorEl={null}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'center',
-                }}
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'center',
-                }}
-              >
-                <MenuItem
-                  style={{ width: '222px' }}
-                  onClick={() => onRequestLogout()}
-                >
-                  Logout
-                </MenuItem>
-              </Menu>
-            </>
-          ) : null}
+          {isAuthUser ? authUserSection : guestSection}
         </div>
       </StyledHeader>
     </Wrapper>
