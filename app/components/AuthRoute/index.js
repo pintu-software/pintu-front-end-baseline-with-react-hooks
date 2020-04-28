00073f59812aae33c16a1bfa7aeac2718746008b
zijunn93 <34432534+zijunn93@users.noreply.github.com>
2020-04-28 13:21:08 +0800

    add auth route

---
+++ app/components/AuthRoute/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect, Route } from 'react-router';
+import { createStructuredSelector } from 'reselect';
+import { makeSelectIsAuthUser } from 'containers/LoginPage/selectors';
+
+const AuthRoute = props => {
+  const { isAuthUser, type } = props;
+  if (type === 'guest' && isAuthUser) {
+    return <Redirect to="/profile" />;
+  }
+
+  if (type === 'private' && !isAuthUser) {
+    return <Redirect to="/" />;
+  }
+
+  return <Route {...props} />;
+};
+
+AuthRoute.propTypes = {
+  isAuthUser: PropTypes.bool,
+  type: PropTypes.string,
+};
+
+const mapStateToProps = createStructuredSelector({
+  isAuthUser: makeSelectIsAuthUser(),
+});
+
+export default connect(mapStateToProps)(AuthRoute);
