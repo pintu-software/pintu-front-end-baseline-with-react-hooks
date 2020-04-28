ff969565c91f93a1fed0bb901737d4f6d222e44e
zijunn93 <34432534+zijunn93@users.noreply.github.com>
2020-04-28 12:34:51 +0800

    initial profile page

---
+++ app/containers/Profile/index.js
@@ -0,0 +1,54 @@
+/**
+ *
+ * Profile
+ *
+ */
+
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import { useInjectSaga } from 'utils/injectSaga';
+import { useInjectReducer } from 'utils/injectReducer';
+import makeSelectProfile from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+import messages from './messages';
+
+export function Profile() {
+  useInjectReducer({ key: 'profile', reducer });
+  useInjectSaga({ key: 'profile', saga });
+
+  return (
+    <div>
+      <FormattedMessage {...messages.header} />
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  profile: makeSelectProfile(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(Profile);
