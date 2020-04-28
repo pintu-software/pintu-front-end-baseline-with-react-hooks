510a0d94a6b73fbb414c977431b8c34723f972e4
zijunn93 <34432534+zijunn93@users.noreply.github.com>
2020-04-28 12:38:41 +0800

    add profile route

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -18,6 +18,7 @@ import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import LoginPage from 'containers/LoginPage/Loadable';
+import ProfilePage from 'containers/Profile/Loadable';
 import GlobalStyle from '../../global-styles';
 
 const AppWrapper = styled.div`
@@ -49,6 +50,7 @@ export default function App() {
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/profile" component={ProfilePage} />
             <Route path="/features" component={FeaturePage} />
             <Route path="" component={NotFoundPage} />
           </Switch>
